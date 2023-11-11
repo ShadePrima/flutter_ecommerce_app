2da8f64a4531895a8133911f3c9ecbbe3c49cc3d
ShadePrima <aleksandrag1984@gmail.com>
2023-11-11 12:31:51 +0200

    add drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -45,7 +45,10 @@ class _HomePageState extends State<HomePage> {
         elevation: 0,
         leading: Builder(builder: (context) {
           return IconButton(
-            icon: const Icon(Icons.menu, color: Colors.black),
+            icon: const Padding(
+              padding: EdgeInsets.only(left: 12),
+              child: Icon(Icons.menu, color: Colors.black),
+            ),
             onPressed: () {
               Scaffold.of(context).openDrawer();
               // Navigator.push(
@@ -54,7 +57,66 @@ class _HomePageState extends State<HomePage> {
           );
         }),
       ),
-      drawer: Drawer(),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                //logo
+                DrawerHeader(
+                    child: Image.asset(
+                  'lib/images/nike.png',
+                  color: Colors.white,
+                  width: 170,
+                )),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Divider(
+                    color: Colors.grey[800],
+                  ),
+                ),
+
+                const Padding(
+                  padding: EdgeInsets.only(left: 25),
+                  child: ListTile(
+                    leading: Icon(Icons.home, color: Colors.white),
+                    title: Text(
+                      "Home",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+
+                const Padding(
+                  padding: EdgeInsets.only(left: 25),
+                  child: ListTile(
+                    leading: Icon(Icons.info, color: Colors.white),
+                    title: Text(
+                      "About",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+
+                //other pages
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 25, bottom: 25),
+              child: ListTile(
+                leading: Icon(Icons.logout, color: Colors.white),
+                title: Text(
+                  "Logout",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: _page[_selectedIndex],
     );
   }
