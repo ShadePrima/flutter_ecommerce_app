741f41c21a2da7459341ae7a064320a6516ad89d
ShadePrima <aleksandrag1984@gmail.com>
2023-11-13 15:10:11 +0200

    add item to cart

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -4,7 +4,8 @@ import '../models/shoe.dart';
 
 class ShoeTile extends StatelessWidget {
   Shoe shoe;
-  ShoeTile({super.key, required this.shoe});
+  void Function()? onTap;
+  ShoeTile({super.key, required this.shoe, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +23,12 @@ class ShoeTile extends StatelessWidget {
               child: Image.asset(shoe.imagePath)),
 
           //description
-          Text(
-            shoe.description,
-            style: TextStyle(color: Colors.grey[600]),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Text(
+              shoe.description,
+              style: TextStyle(color: Colors.grey[600]),
+            ),
           ),
 
           //price + details
@@ -57,17 +61,20 @@ class ShoeTile extends StatelessWidget {
                 ),
 
                 //plus button
-                Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: const BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(12),
-                            bottomRight: Radius.circular(12))),
-                    child: const Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ))
+                GestureDetector(
+                  onTap: onTap,
+                  child: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: const BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(12),
+                              bottomRight: Radius.circular(12))),
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      )),
+                )
               ],
             ),
           )
